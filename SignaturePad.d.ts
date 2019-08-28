36b7516b7f292231408d4ff815ae69c9ce2286b2
Leo <leonard@trunkrs.nl>
2019-08-28 17:06:33 +0200

    Added typescript support

---
+++ SignaturePad.d.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ViewProps } from 'react-native'
+
+export interface SignatureResult {
+  encoded: string
+  pathName: string
+}
+
+export interface SignaturePadProps extends ViewProps{
+  rotateClockwise?: boolean
+  square?: boolean
+  saveImageFileInExtStorage?: boolean
+  viewMode?: "portrait" | "landscape"
+  showBorder?: boolean
+  showNativeButtons?: boolean
+  showTitleLabel?: boolean
+  maxSize?: number
+  minStrokeWidth?: number
+  maxStrokeWidth?: number
+  strokeColor?: string
+  backgroundColor?: string
+  compressionQuality?: number
+  outputFormat?: "png" | "jpg"
+  onSave?: (event: SignatureResult) => void | Promise<void>
+  onDragStart?: () => void | Promise<void>
+  onDragEnd?: () => void | Promise<void>
+}
+
+declare const SignaturePad: React.Component<SignaturePadProps>
+
+export default SignaturePad
